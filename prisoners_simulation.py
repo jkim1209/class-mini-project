1c162b87e8b152d09cb4420b8c795c3f147c46e6
jkim1209 <jwk143@gmail.com>
2025-06-27 18:15:18 +0900

    feat: Added random strategy function play_random

---
+++ prisoners_simulation.py
@@ -0,0 +1,21 @@
+import random
+
+def play_random(trials=100000):
+    pardoned = 0
+    in_drawer = list(range(100))
+    sampler = list(range(100))
+    for _ in range(trials):
+        random.shuffle(in_drawer)
+        found = False
+        for prisoner in range(100):
+            found = False
+            for reveal in random.sample(sampler, 50):
+                card = in_drawer[reveal]
+                if card == prisoner:
+                    found = True
+                    break
+            if not found:
+                break
+        if found:
+            pardoned += 1
+    return pardoned / trials * 100
